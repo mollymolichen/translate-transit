f46d164b9611181b0be3ebc80f81fc20f53c52f6
Molly Chen <m.swimstar@gmail.com>
2018-09-17 10:35:24 -0400

    Add dist folder after build

---
+++ transit-angular/landmarks.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { TranslateService } from '../translate.service';
+declare var require: any;
+var distanceJSON = require('../distance-to-from.json');
+import * as _ from "lodash";
+import { LocationComponent } from '../location/location';
+
+@Component({
+  selector: 'page-landmarks',
+  templateUrl: 'landmarks.html'
+})
+export class LandmarksComponent {
+  myLocation = localStorage.getItem('myLocation') ? localStorage.getItem('myLocation') : 'N/A';
+  distanceList = JSON.parse(localStorage.getItem('distanceList')) ? JSON.parse(localStorage.getItem('distanceList')) : [];
+
+  constructor(private translateService: TranslateService) {
+    
+  }
+
+  ngOnInit(){
+    this.getDistances();
+  }
+
+  getDistances(){
+    let myLoc = localStorage.getItem('myLocation') ? localStorage.getItem('myLocation') : 'N/A';
+    
+    for (let i = 0; i < distanceJSON.length; i++){
+      if (distanceJSON[i].source === myLoc){
+        this.distanceList.push(distanceJSON[i]);
+      }
+    }
+    console.log(this.distanceList);
+    localStorage.setItem('distanceList', JSON.stringify(this.distanceList));
+    return this.distanceList;
+  }
+
+  // updateDistanceView(){
+  //   this.distanceList = localStorage.getItem('distanceList') ? JSON.parse(localStorage.getItem('distanceList')) : null;
+  // }
+}
+
+
